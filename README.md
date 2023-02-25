a7c906db4941b368f970131781e1466e66b7b810
crazymugg <78958134+crazymugg@users.noreply.github.com>
2023-02-25 14:42:48 +0100

    Updated documentation
    also added minor error handling checks

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MsWordUpdater
-Python script to update a batch of ms word documents
+Python script to update a batch of ms word documents (must end in .docx)
 
 step 1:
 Update 'data/update_list' to change any text you want to update from old '.docx' files
@@ -19,3 +19,5 @@ NOTES:
 As of now, anytime the text of a phrase gets updated, that text's formatting (Bold, Font, FontSize, etc.) gets over-written back to the documents default. Will add the ability to retain formatting in the future.
 
 Issues with Table of Contents of a document getting deleted if a section title gets updated. Will need ot investigate this...
+
+footer_loop, header_loop, and table_loop are very similar. Will look into consolidating into 1 single method
--- main.py
+++ main.py
@@ -9,58 +9,6 @@ import docx
 from data.update_list import data
 
 
-
-def match_loop(i, data):
-    match = False
-
-    # Cycle through all key,value pairs of data
-    for key, value in data.items():
-
-        # Setting the text in the document to a variable
-        textline = doc.paragraphs[int(i)].text
-        print(textline)
-
-        # Searching that variable for the given key, if match returns the index where it occurs
-        start_index = textline.find(key)
-        end_index = start_index + len(key)
-
-        # If there is a key match
-        if start_index != -1:
-
-            # If the match is at the start of the textline
-            if start_index == 0:
-                logging.warning(f'Match at paragraph {i} for {key} at the start')
-                match = True
-
-                splits = textline.split(key)
-                new_line = value + splits[1]
-                doc.paragraphs[i].text = new_line
-
-            # If the match is at the end of the textline
-            elif end_index == len(textline):
-                logging.warning(f'Match at paragraph {i} for {key} at the end')
-                match = True
-
-                splits = textline.split(key)
-                new_line = splits[0] + value
-                doc.paragraphs[i].text = new_line
-
-            # If the match is somewhere in the middle
-            else:
-                logging.warning(f'Match at paragraph {i} for {key} in the middle')
-                match = True
-
-                splits = textline.split(key)
-                new_line = splits[0] + value + splits[1]
-                doc.paragraphs[i].text = new_line
-
-    # If no match for the given paragraph
-    if match == False:
-        print('\n # NO MATCH # \n')
-
-
-
-
 class Text():
     def __init__(self):
         # Future TODO, setup the ability for text formatting memorization.
@@ -74,7 +22,7 @@ class Text():
     def text_check(phrase, dic):
         # If phrase is blank, skip the check
         if phrase == None or phrase == '':
-            pass
+            return phrase
 
         else:
             # Cycle through all key,value pairs of data
@@ -109,11 +57,6 @@ class Text():
 
 
 
-
-
-
-
-
 class Word():
     def __init__(self, file):
         self.old_name = file
@@ -141,36 +84,66 @@ class Word():
 
     # Loop through the document's Headers
     def header_loop(self):
-        #self.doc.sections[0].header.tables[0]._cells
+        # Loop through all tables on the header
         i = 0
-        # Loop through the tables on the header
-        while i < len(self.doc.sections[0].header.tables):
+        while i < len(self.doc.sections[0].header.tables): 
             print(f'\n Checking Header Table {i} \n')
+
+            # Loop through all cells of the table
             j = 0
-            # Loop through the cells of the table
             while j < len(self.doc.sections[0].header.tables[i]._cells):
+
+                # Setting current text to var 'text_line'
                 text_line = self.doc.sections[0].header.tables[i]._cells[j].text
-                if text_line == '' or text_line == None:
-                    pass
-                else:
+
+                # Check if text of the cell exists. If not, skip to next cell
+                if text_line != '' and text_line != None:
+
+                    # Creating new updated phrase from the update_list 
                     new_textline = Text.text_check(phrase=text_line, dic=self.data)
+
+                    # Check to see if the new phrase is the exact same as the old phrase
+                    # Due to text formatting over-writting issues, It is best to avoid updating text on the document that doesn't need to be updated. 
                     if new_textline != text_line:
-                        self.doc.sections[0].header.tables[i]._cells[j].text = new_textline
-                        self.log_change(content=f'{new_textline} /// on header table {i}, cell {j}')
+
+                        # Update the Header with the new updated phrase
+                        try:
+                            self.doc.sections[0].header.tables[i]._cells[j].text = new_textline
+                            self.log_change(content=f'{new_textline} /// on header table {i}, cell {j}') # Log this change
+                        except:
+                            logging.error('Header could not be looped thru!') # Error handling if docx can't be updated
+                            self.log_change(content=f' E R R O R! on header table {i}, cell {j}') # Log this error
                 j += 1
             i += 1
 
 
     # Loop through the document's Paragraphs
     def paragraph_loop(self):
+        # Loop through all paragraphs in the document
         i = 0
         while i < len(self.doc.paragraphs):
             print(f'\n Checking Paragraph {i} \n')
+
+            # Setting current text to var 'text_line'
             text_line = self.doc.paragraphs[int(i)].text
-            new_textline = Text.text_check(phrase=text_line, dic=self.data)
-            if new_textline != text_line:
-                self.doc.paragraphs[i].text = new_textline
-                self.log_change(content=f'{new_textline} on paragraph {i}')
+
+            # Check if text exists. If not, skip to next paragraph
+            if text_line != '' and text_line != None:
+
+                # Creating new updated phrase from the update_list 
+                new_textline = Text.text_check(phrase=text_line, dic=self.data)
+
+                # Check to see if the new phrase is the exact same as the old phrase
+                # Due to text formatting over-writting issues, It is best to avoid updating text on the document that doesn't need to be updated. 
+                if new_textline != text_line:
+
+                    # Update the Paragraph with the new updated phrase
+                    try:
+                        self.doc.paragraphs[i].text = new_textline
+                        self.log_change(content=f'{new_textline} on paragraph {i}') # Log this change
+                    except:
+                        logging.error('Paragraph could not be looped thru!') # Error handling if docx can't be updated
+                        self.log_change(content=f' E R R O R! on paragraph {i}!') # Log this error
             i += 1
 
 
@@ -179,52 +152,74 @@ class Word():
         i = 0
         # Loop through the tables on the doc
         while i < len(self.doc.tables):
-            #if i == 6:
-                #test = 'test'
-                #print('test')
             print(f'\n Checking Table {i} \n')
+
+            # Loop through all cells of the table
             j = 0
-            # Loop through the cells of the table
             while j < len(self.doc.tables[i]._cells):
+
+                # Setting current text to var 'text_line'
                 text_line = self.doc.tables[i]._cells[j].text
-                print(text_line)
-                if text_line == '' or text_line == None:
-                    pass
-                else:
+                
+                # Check if text of the cell exists. If not, skip to next cell
+                if text_line != '' and text_line != None:
+
+                    # Creating new updated phrase from the update_list
                     new_textline = Text.text_check(phrase=text_line, dic=self.data)
-                    print(new_textline)
+
+                    # Check to see if the new phrase is the exact same as the old phrase
+                    # Due to text formatting over-writting issues, It is best to avoid updating text on the document that doesn't need to be updated. 
                     if new_textline != text_line:
+
+                        # Update the Table with the new updated phrase
                         try:
                             self.doc.tables[i]._cells[j].text = new_textline
-                            self.log_change(content=f'{new_textline} on body table {i}, cell {j}')
+                            self.log_change(content=f'{new_textline} on body table {i}, cell {j}') # Log this error
                         except:
-                            logging.error('Table could not be looped thru!')
-                            self.log_change(content=f' E R R O R! on body table {i}, cell {j}')
+                            logging.error('Table could not be looped thru!') # Error handling if docx can't be updated
+                            self.log_change(content=f' E R R O R! on body table {i}, cell {j}') # Log this error
                 j += 1
             i += 1
+
+
 
 
     # Loop through the document's Footers
     def footer_loop(self):
         i = 0
-        # Loop through the tables on the header
+        # Loop through the tables on the Footer
         while i < len(self.doc.sections[0].footer.tables):
             print(f'\n Checking Footer Table {i} \n')
-            j = 0
+
             # Loop through the cells of the table
+            j = 0
             while j < len(self.doc.sections[0].footer.tables[i]._cells):
+
+                # Setting current text to var 'text_line'
                 text_line = self.doc.sections[0].footer.tables[i]._cells[j].text
-                if text_line == '' or text_line == None:
-                    pass
-                else:
+
+                # Check if text of the cell exists. If not, skip to next cell
+                if text_line != '' and text_line != None:
+
+                    # Creating new updated phrase from the update_list
                     new_textline = Text.text_check(phrase=text_line, dic=self.data)
+
+                    # Check to see if the new phrase is the exact same as the old phrase
+                    # Due to text formatting over-writting issues, It is best to avoid updating text on the document that doesn't need to be updated. 
                     if new_textline != text_line:
-                        self.doc.sections[0].footer.tables[i]._cells[j].text = new_textline
-                        self.log_change(content=f'{new_textline} on footer table {i}, cell {j}')
+
+                    # Update the Table with the new updated phrase
+                        try:
+                            self.doc.sections[0].footer.tables[i]._cells[j].text = new_textline # Log this error
+                            self.log_change(content=f'{new_textline} on footer table {i}, cell {j}')
+                        except:
+                            logging.error('Table could not be looped thru!') # Error handling if docx can't be updated
+                            self.log_change(content=f' E R R O R! on body table {i}, cell {j}') # Log this error
                 j += 1
             i += 1
 
 
+    # For each document, loop through all sections, with the below methods
     def loop_thru_document(self):
         self.header_loop()
         self.paragraph_loop()
@@ -236,33 +231,60 @@ class Word():
 
 class App():
     def __init__(self):
-        path = './input'
-        self.name_list = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-        print(self.name_list)
+        self.input_path = './input'
+        self.filename_list = self.get_files()
 
 
+    # Finds and registers all files in the input folder
+    def get_files(self):
+        filelist = []
+
+        # registers the input directory 
+        dirpath = os.listdir(self.input_path)
+
+        # Loop through the input directory
+        for file in dirpath:
+
+            # Checks if file is valid (aka, not a subfolder)
+            if os.path.isfile(os.path.join(self.input_path, file)):
+
+                # Adds the file to the file list
+                filelist.append(file)
+
+        # return the finalized file list
+        print(filelist)
+        return filelist
+
+
+    # Deletes log file if existing
     def delete_old_log(self):
-        if os.path.exists('log.txt'):
-            os.remove('log.txt')
+        if os.path.exists('data/log.txt'):
+            os.remove('data/log.txt')
         else:
             print('Log file not found')
 
 
+    # Main scripting loop through all the files
     def main_loop(self):
         self.delete_old_log()
-        for file in self.name_list:
+        
+        # loop through each file in the input folder
+        for file in self.filename_list:
             print(f'\n Loading {file} \n')
+
+            # Initialize document as a word file with the script
             word = Word(file)
+
+            # Loop through the given document
             word.loop_thru_document()
-            #test = word.doc.sections[0].header.tables
-            #print(test)
+
+            # Save the document after the loop is completed in the output folder
             word.save_doc()
 
 
 
 
+# Start main loop when file is run
 if __name__ == '__main__':
     app = App()
     app.main_loop()
-    #for name in app.name_list:
-        #print(name)
